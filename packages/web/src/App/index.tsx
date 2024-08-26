8d1d6ab0540fd5311837462f1c539078bcb2ab62
Xu Yiming <xuyiming.open@outlook.com>
2024-08-26 17:12:24 +0800

    revert(web): Remove demo

--- packages/web/src/App/index.tsx
+++ packages/web/src/App/index.tsx
@@ -1,10 +1,5 @@
-import "./index.css";
-
 function App() {
-	return <div class="content">
-		<h1>Rspack</h1>
-		<p>Start building amazing things with Rspack.</p>
-	</div>;
+	return <></>;
 }
 
 export default App;
--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,6 +1,5 @@
 import { render } from "preact";
 import App from "./App";
-import "./index.css";
 
 if (import.meta.nodeEnv === "development") {
 	await import('preact/debug')
