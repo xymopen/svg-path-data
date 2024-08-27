e896fb43726203cfd656afadf5d53327bd8967ca
Xu Yiming <xuyiming.open@outlook.com>
2024-08-27 16:33:08 +0800

    feat(web): Stringify SVG Path-Data

--- packages/web/src/App/index.tsx
+++ packages/web/src/App/index.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useRef } from "preact/hooks";
 import ClevoLogo from "assets/Clevo_logo.svg";
 import { fromIt } from "parser-combinators";
-import svgPath from "svg-path-data";
+import svgPath, { AllCommand } from "svg-path-data";
+import stringifyCommand from "svg-path-data/stringify";
 
 function App() {
 	const divRef = useRef<HTMLDivElement>(null);
@@ -12,6 +13,13 @@ function App() {
 			Array.prototype.forEach.call(svg.querySelectorAll("path"), (path: SVGPathElement) => {
 				const pathData = path.getAttribute("d")!;
 				const [remaining, svgPathData] = svgPath(fromIt(pathData));
+				let s = '';
+				let previous: AllCommand | undefined = undefined;
+				for (let current of svgPathData) {
+					s += stringifyCommand(current, previous);
+					previous = current;
+				}
+				path.setAttribute('d', s);
 				console.log(Array.from({ [Symbol.iterator]() { return { next: remaining } } }).join(''));
 			});
 		}
