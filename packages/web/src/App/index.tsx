9f3667a32ecde807dc0caa1d9b16c63456ef071f
Xu Yiming <xuyiming.open@outlook.com>
2024-08-27 16:09:09 +0800

    chore(web): Add SVG asset

--- packages/web/src/App/index.tsx
+++ packages/web/src/App/index.tsx
@@ -1,5 +1,18 @@
+import { Component, JSX, Ref } from "preact";
+import { useEffect, useRef } from "preact/hooks";
+import ClevoLogo from "assets/Clevo_logo.svg";
+
 function App() {
-	return <></>;
+	const svgRef = useRef<typeof ClevoLogo>(null);
+
+	useEffect(() => {
+		if (svgRef.current != null) {
+			// Preact will treat function component as class component at runtime
+			const svg = (svgRef.current as unknown as Component<JSX.IntrinsicSVGElements["svg"]>).base as SVGSVGElement;
+		}
+	}, [svgRef]);
+
+	return <ClevoLogo ref={svgRef as Ref<any>} />;
 }
 
 export default App;
