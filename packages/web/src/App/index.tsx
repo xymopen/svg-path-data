3db07cf381131d34110b58b5a03fa459f4d06167
Xu Yiming <xuyiming.open@outlook.com>
2024-08-27 16:25:43 +0800

    feat(web): Parse SVG Path-Data

--- packages/web/src/App/index.tsx
+++ packages/web/src/App/index.tsx
@@ -1,6 +1,8 @@
 import { Component, JSX, Ref } from "preact";
 import { useEffect, useRef } from "preact/hooks";
 import ClevoLogo from "assets/Clevo_logo.svg";
+import { fromIt } from "parser-combinators";
+import svgPath from "svg-path-data";
 
 function App() {
 	const svgRef = useRef<typeof ClevoLogo>(null);
@@ -9,6 +11,11 @@ function App() {
 		if (svgRef.current != null) {
 			// Preact will treat function component as class component at runtime
 			const svg = (svgRef.current as unknown as Component<JSX.IntrinsicSVGElements["svg"]>).base as SVGSVGElement;
+			Array.prototype.forEach.call(svg.querySelectorAll("path"), (path: SVGPathElement) => {
+				const pathData = path.getAttribute("d")!;
+				const [remaining, svgPathData] = svgPath(fromIt(pathData));
+				console.log(Array.from({ [Symbol.iterator]() { return { next: remaining } } }).join(''));
+			});
 		}
 	}, [svgRef]);
 
